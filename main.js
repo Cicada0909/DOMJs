869e0651f1acb50debe31551ac47bb7d8c269152
Alibek Tauirbaev <featternotving@gmail.com>
2024-12-11 08:11:18 +0500

    corection

--- main.js
+++ main.js
@@ -11,34 +11,3 @@ userDescription.textContent = prompt("Введите кратку информа
 userProfile.append(userName);
 
 userProfile.append(userDescription);
-
-
-
-
-// let userColor = prompt("введите цвет");
-
-// // box.classList.add(userColor);
-
-// // const newBox = document.createElement("div");
-
-// // newBox.textContent = "New Box from Js";
-
-// // newBox.classList.add("red")
-
-// // // const body = document.querySelector("Body");
-// // const body = document.body;
-
-// // console.log(newBox);
-
-// // body.appendChild(newBox);
-// // // body.append() - универсальнее?
-
-// const container = document.querySelector(".container");
-
-// for (let i = 0; i < 10; i++) {
-//     const newElem = document.createElement("div");
-
-//     newElem.textContent = i + 1;
-//     newElem.classList.add("box");
-
-//     container.append(newElem);
